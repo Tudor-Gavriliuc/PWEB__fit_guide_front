e6db0cfda0282989782d206034ffbd33d00c4930
Tudor_889 <sigmoidtudor@gmail.com>
2025-05-25 22:02:14 +0300

    added pagination

--- src/pages/MuscleDetail.jsx
+++ src/pages/MuscleDetail.jsx
@@ -1,8 +1,5 @@
 import { useParams } from 'react-router-dom';
 import { muscles } from '../data/muscles';
-import chestExercises from '../data/chestExercises.js';
-import backExercises from '../data/backExercises.js';
-import legsExercises from '../data/legsExercises.js';
 import ExerciseCard from '../components/ExerciseCard';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
@@ -10,19 +7,29 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import AddExerciseForm from '../components/AddExerciseForm';
 
-export default function MuscleDetail({ toggleBackground, bgMode, likedExercises, toggleLike, showOnlyLiked, toggleShowOnlyLiked }) {
+export default function MuscleDetail({
+  toggleBackground,
+  bgMode,
+  likedExercises,
+  toggleLike,
+  showOnlyLiked,
+  toggleShowOnlyLiked
+}) {
   const { id } = useParams();
   const muscle = muscles.find(m => m.id === id);
-  const [exercises, setExercises] = useState({});
+  const [exercises, setExercises] = useState([]);
+  const [totalExercises, setTotalExercises] = useState(0);
   const [showAddExercise, setShowAddExercise] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const [currentExercise, setCurrentExercise] = useState(null);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const cardsPerPage = 6;
 
   useEffect(() => {
     if (!muscle) return;
 
     let apiUrl = '';
-
     switch (muscle.id) {
       case 'chest':
         apiUrl = 'http://127.0.0.1:8000/api/get_chest_exercises';
@@ -36,72 +43,89 @@ export default function MuscleDetail({ toggleBackground, bgMode, likedExercises,
     }
 
     if (apiUrl) {
-      axios.get(apiUrl)
+      axios
+        .get(apiUrl, {
+          params: {
+            page: currentPage,
+            page_size: cardsPerPage
+          }
+        })
         .then(response => {
-          setExercises(response.data);
+          setExercises(response.data.results);
+          setTotalExercises(response.data.total);
         })
         .catch(error => {
           console.error('API error:', error);
         });
     }
-  }, [muscle]);
+  }, [muscle, currentPage]);
 
-  const handleDelete = async (id) => {
-      if (!window.confirm('Are you sure you want to delete this exercise?')) return;
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [showOnlyLiked]);
 
-      try {
-        const response = await fetch(`http://127.0.0.1:8000/api/delete_exercise/${id}`, {
-          method: 'DELETE',
-        });
+  const handleDelete = async id => {
+    if (!window.confirm('Are you sure you want to delete this exercise?')) return;
 
-        if (!response.ok) throw new Error('Failed to delete');
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/delete_exercise/${id}`, {
+        method: 'DELETE'
+      });
 
-        window.location.reload();
-      } catch (error) {
-        console.error('Delete failed:', error);
-      }
-    };
+      if (!response.ok) throw new Error('Failed to delete');
 
-    const handleFormClose = async (id) => {
-        setShowAddExercise(false);
-        window.location.reload();
-    };
+      window.location.reload();
+    } catch (error) {
+      console.error('Delete failed:', error);
+    }
+  };
 
-    const handleEdit = (exerciseId) => {
-      const exerciseToEdit = exercises.find(e => e.id === exerciseId);
-      setCurrentExercise(exerciseToEdit);
-      setIsEditing(true);
-    };
+  const handleFormClose = () => {
+    setShowAddExercise(false);
+    window.location.reload();
+  };
 
-    const handleUpdate = async () => {
-      const formData = new FormData();
-      formData.append('name', currentExercise.name);
-      formData.append('description', currentExercise.description);
-      formData.append('muscle_group', currentExercise.muscle_group);
-      formData.append('id', currentExercise.id);
+  const handleEdit = exerciseId => {
+    const exerciseToEdit = exercises.find(e => e.id === exerciseId);
+    setCurrentExercise(exerciseToEdit);
+    setIsEditing(true);
+  };
 
-      if (currentExercise.image instanceof File) {
-        formData.append('image', currentExercise.image);
-      }
+  const handleUpdate = async () => {
+    const formData = new FormData();
+    formData.append('name', currentExercise.name);
+    formData.append('description', currentExercise.description);
+    formData.append('muscle_group', currentExercise.muscle_group);
+    formData.append('id', currentExercise.id);
 
-      try {
-        const response = await fetch('http://127.0.0.1:8000/api/update_exercise', {
-          method: 'POST',
-          body: formData,
-        });
+    if (currentExercise.image instanceof File) {
+      formData.append('image', currentExercise.image);
+    }
 
-        if (!response.ok) throw new Error('Failed to update');
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/update_exercise', {
+        method: 'POST',
+        body: formData
+      });
 
-        const data = await response.json();
-        console.log('Updated:', data);
-        window.location.reload();
-        setEditingExercise(null);
-      } catch (error) {
-        console.error('Update error:', error);
-      }
-};
+      if (!response.ok) throw new Error('Failed to update');
+
+      const data = await response.json();
+      console.log('Updated:', data);
+      window.location.reload();
+      setIsEditing(false);
+    } catch (error) {
+      console.error('Update error:', error);
+    }
+  };
 
   if (!muscle) return <p className="text-center mt-4">Grupa musculară nu a fost găsită.</p>;
+
+  const filteredExercises = exercises.filter(
+    ex => !showOnlyLiked || likedExercises.includes(ex.id)
+  );
+
+  const totalPages = Math.ceil(totalExercises / cardsPerPage);
 
   return (
     <div className="d-flex flex-column min-vh-100">
@@ -114,59 +138,69 @@ export default function MuscleDetail({ toggleBackground, bgMode, likedExercises,
       />
 
       <main className="flex-grow-1">
-          {!isEditing && showAddExercise && (
-              <div className="modal-content p-4 bg-white rounded shadow" style={{ maxWidth: 600, margin: '2rem auto' }}>
-                <button
-                  className="btn btn-sm btn-outline-danger mb-3"
-                  onClick={() => handleFormClose()}
-                >
-                  Închide
-                </button>
-                <AddExerciseForm />
-              </div>
-          )}
-            {isEditing && currentExercise && (
-            <div className="modal-content p-4 bg-white rounded shadow" style={{ maxWidth: 600, margin: '2rem auto' }}>
-              <form
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  handleUpdate(currentExercise);
-                  setIsEditing(false);
-                }}
-                className="mt-4"
+        {!isEditing && showAddExercise && (
+          <div
+            className="modal-content p-4 bg-white rounded shadow"
+            style={{ maxWidth: 600, margin: '2rem auto' }}
+          >
+            <button
+              className="btn btn-sm btn-outline-danger mb-3"
+              onClick={handleFormClose}
+            >
+              Închide
+            </button>
+            <AddExerciseForm />
+          </div>
+        )}
+
+        {isEditing && currentExercise && (
+          <div
+            className="modal-content p-4 bg-white rounded shadow"
+            style={{ maxWidth: 600, margin: '2rem auto' }}
+          >
+            <form
+              onSubmit={e => {
+                e.preventDefault();
+                handleUpdate();
+              }}
+              className="mt-4"
+            >
+              <label htmlFor="name" className="form-check-label">
+                Exercise Name
+              </label>
+              <input
+                id="name"
+                type="text"
+                value={currentExercise.name}
+                onChange={e =>
+                  setCurrentExercise({ ...currentExercise, name: e.target.value })
+                }
+                className="form-control mb-2"
+                placeholder="Exercise name"
+              />
+
+              <button type="submit" className="btn btn-primary btn-sm">
+                Save
+              </button>
+              <button
+                type="button"
+                className="btn btn-secondary btn-sm ms-2"
+                onClick={() => setIsEditing(false)}
               >
-                <label htmlFor="name" className="form-check-label">Exercise Name</label>
-                <input
-                  id="name"
-                  type="text"
-                  value={currentExercise.name}
-                  onChange={(e) =>
-                    setCurrentExercise({ ...currentExercise, name: e.target.value })
-                  }
-                  className="form-control mb-2"
-                  placeholder="Exercise name"
-                />
+                Cancel
+              </button>
+            </form>
+          </div>
+        )}
 
-                <button type="submit" className="btn btn-primary btn-sm">
-                  Save
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-secondary btn-sm ms-2"
-                  onClick={() => setIsEditing(false)}
-                >
-                  Cancel
-                </button>
-              </form>
-              </div>
-            )}
-
-        <div className="container-fluid" style={{ maxWidth: '1300px', margin: '0 auto', padding: '0 2rem' }}>
-          {!isEditing && exercises.length > 0 && !showAddExercise&& (
-            <div className="row g-4">
-              {exercises
-                .filter(ex => !showOnlyLiked || likedExercises.includes(ex.id))
-                .map(exercise => (
+        <div
+          className="container-fluid"
+          style={{ maxWidth: '1300px', margin: '0 auto', padding: '0 2rem' }}
+        >
+          {!isEditing && !showAddExercise && filteredExercises.length > 0 && (
+            <>
+              <div className="row g-4">
+                {filteredExercises.map(exercise => (
                   <ExerciseCard
                     key={exercise.id}
                     exercise={exercise}
@@ -176,7 +210,30 @@ export default function MuscleDetail({ toggleBackground, bgMode, likedExercises,
                     handleEdit={handleEdit}
                   />
                 ))}
-            </div>
+              </div>
+
+              {totalPages > 1 && (
+                <div className="d-flex justify-content-center my-4">
+                  <nav>
+                    <ul className="pagination">
+                      {Array.from({ length: totalPages }, (_, index) => (
+                        <li
+                          key={index}
+                          className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}
+                        >
+                          <button
+                            className="page-link"
+                            onClick={() => setCurrentPage(index + 1)}
+                          >
+                            {index + 1}
+                          </button>
+                        </li>
+                      ))}
+                    </ul>
+                  </nav>
+                </div>
+              )}
+            </>
           )}
         </div>
       </main>
